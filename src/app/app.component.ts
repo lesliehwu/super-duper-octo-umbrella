be5e16641b0c588d588734096078575e208f5078
lesliehwu <mslesliewu@gmail.com>
2017-10-24 17:07:31 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    emails=[
+        {email:'sadie@pomeranian.com',importance:true,subject:'I AM CUTE',content:'bork bork bork'},
+        {email:'fifi@poodle.com',importance:false,subject:'Heavy Metal Playlist',content:'boof'},
+        {email:'hugo@bulldog.com',importance:true,subject:'Leslie\'s dog',content:'wooooof'},
+        {email:'deedles@lhasa.apso',importance:false,subject:'so cute',content:'wooofer'}
+    ];
 }
